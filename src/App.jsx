d3121b96fc8914f4eca1b76065c109345c74a888
kishore-rao-k <kishoreraok12@gmail.com>
2025-04-03 11:10:59 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/navBar/NavBar"; 
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1>Welcome </h1>
+    </>
+  );
+}
+
+export default App;
